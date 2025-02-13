3b61c793618b251b655a033f3b06ee5a6b5da42a
Aleksey Zhuravishkin <zhuravishkin@gmail.com>
2025-02-13 14:00:00 +0300

    added DiscreteAcceleration

--- src/main/java/com/zhuravishkin/courses/demo/Test.java
+++ src/main/java/com/zhuravishkin/courses/demo/Test.java
@@ -2,43 +2,14 @@ package com.zhuravishkin.courses.demo;
 
 import lombok.Data;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 @Data
 public class Test {
     public static void main(String[] args) {
-        String name;
-        Object object;
-        List<String> list = new LinkedList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        List<String> list2 = list;
-        list2.remove("b");
-        System.out.println(list);
-        System.out.println(list2);
-        Map<String, String> map = new HashMap<>();
-
-//        Object[] objects = new String[2];
-//        objects[0] = "test";
-////        objects[1] = 1;
-//        for (int i = 0; i < objects.length; i++) {
-//            System.out.println(objects[i]);
-//        }
-//
-//        String[] strings = new String[0];
-//        strings[0] = "test";
-
-//        int a = 150;
-//        byte b = (byte) a;
-//        System.out.println(b);
-//        System.out.println(Integer.toBinaryString(150));
-
-//        RuntimeException runtimeException = new RuntimeException();
-//        out.println("hello");
-//        throw runtimeException;
+        int a = 0;
+        int b = 9;
+        double aa = a;
+        double bb = b;
+        double c = (b - aa) / 2;
+        System.out.println(c);
     }
 }
--- src/test/java/com/zhuravishkin/courses/leetcode/binary_search/DiscreteAccelerationTest.java
+++ src/test/java/com/zhuravishkin/courses/leetcode/binary_search/DiscreteAccelerationTest.java
@@ -2,24 +2,37 @@ package com.zhuravishkin.courses.leetcode.binary_search;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DiscreteAccelerationTest {
 
     @Test
-    void stringMatching1() {
-        assertLinesMatch(List.of("as", "hero"), DiscreteAcceleration.stringMatching(new String[]{"mass", "as", "hero", "superhero"}));
+    void run1() {
+        assertEquals(3.0, DiscreteAcceleration.run(10, new int[]{1, 9}));
     }
 
     @Test
-    void stringMatching2() {
-        assertLinesMatch(List.of("et", "code"), DiscreteAcceleration.stringMatching(new String[]{"leetcode", "et", "code"}));
+    void run2() {
+        assertEquals(3.666666666666667, DiscreteAcceleration.run(10, new int[]{1}));
     }
 
     @Test
-    void stringMatching3() {
-        assertLinesMatch(List.of(), DiscreteAcceleration.stringMatching(new String[]{"blue", "green", "bu"}));
+    void run3() {
+        assertEquals(2.047619047619048, DiscreteAcceleration.run(7, new int[]{1, 2, 3, 4, 6}));
+    }
+
+    @Test
+    void run4() {
+        assertEquals(329737645.750000000000000, DiscreteAcceleration.run(1000000000, new int[]{413470354, 982876160}));
+    }
+
+    @Test
+    void run5() {
+        assertEquals(53.700000000000000, DiscreteAcceleration.run(478, new int[]{1, 10, 25, 33, 239, 445, 453, 468, 477}));
+    }
+
+    @Test
+    void run6() {
+        assertEquals(5.0, DiscreteAcceleration.run(10, new int[]{}));
     }
 }
